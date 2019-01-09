008e797f8290adee9d75e333f63134cfb0bfb7e7
Justin Crandall <jcrandall@coaliance.com>
2019-01-09 03:56:52 -0800

    Auth and Wake

--- Tsla.Works/Controllers/HomeController.cs
+++ Tsla.Works/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Tsla.Works.Models;
 
 namespace Tsla.Works.Controllers
@@ -12,7 +13,49 @@ namespace Tsla.Works.Controllers
     {
         public IActionResult Index()
         {
+            TeslaOAuthResponse result = null;
+            
+            if (Request.Cookies["tsla.works"] != null)
+            {
+                string cookievalue = Request.Cookies["tsla.works"];
+                TeslaOAuthResponse response = JsonConvert.DeserializeObject<TeslaOAuthResponse>(cookievalue);
+            }
+            else
+            {
+                result = Task.Run<TeslaOAuthResponse>(async () => await AccountController.Authenticate()).Result;
+                string cookie = JsonConvert.SerializeObject(result);
+                Response.Cookies.Append("tsla.works", cookie);
+            }            
+
             return View();
+        }
+
+        private TeslaOAuthResponse CookieCheck()
+        {
+            TeslaOAuthResponse authResponse = null;
+            if (Request.Cookies["tsla.works"] != null)
+            {
+                string cookievalue = Request.Cookies["tsla.works"];
+                authResponse = JsonConvert.DeserializeObject<TeslaOAuthResponse>(cookievalue);
+            }
+            else
+            {
+                //RedirectToAction("Login", "Account");
+            }
+
+            return authResponse;
+        }
+
+        [HttpPost]
+        public JsonResult Wake()
+        {
+            TeslaOAuthResponse authResponse = CookieCheck();
+
+            bool result = Task.Run<bool>(async () => await AccountController.Wake()).Result;
+
+            JsonResult json = new JsonResult(null);
+
+            return json;
         }
 
         public IActionResult About()
---
+++ Tsla.Works/Models/TeslaOAuth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tsla.Works.Models
+{
+    public class TeslaOAuth
+    {
+        public string grant_type { get; set; }
+        public string client_id { get; set; }
+        public string client_secret { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+
+    public class TeslaOAuthResponse
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string expires_in { get; set; }
+        public string refresh_token { get; set; }
+        public string created_at { get; set; }
+    }
+}
